685719416d06ac7add7f81ca1322de2483a6d24a
syedkhizarulhaq-cis <syedkhizarulhaq@yahoo.com>
2018-08-22 15:00:03 +0500

    no comments

--- start_2.py
+++ start_2.py
@@ -17,6 +17,19 @@ dates = pd.date_range('20130101', periods=6)
 
 df = pd.DataFrame(np.random.randn(6,4), index=dates, columns=list('ABCD'))
 
-print(df)
+#print(df)
 
+#Creating a DataFrame by passing a dict of objects that can be converted to series-like.
+df2 = pd.DataFrame({ 'A' : 1.,
+                         'B' : pd.Timestamp('20130102'),
+                         'C' : pd.Series(1,index=list(range(4)),dtype='float32'),
+                         'D' : np.array([3] * 4,dtype='int32'),
+                         'E' : pd.Categorical(["test","train","test","train"]),
+                         'F' : 'foo' })
+#print(df2)
 
+#print(df2.dtypes)
+
+#print(df.head())
+
+#print(df.tail())
